49d33972f2d0b059d5842031ec6303306ae8cbf3
Andrew L <anleac@github.com>
2023-09-22 20:28:05 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# defend_your_flame
+
+Inspired by a classic game I grew up with.
---
+++ android/app/src/main/kotlin/com/example/defend_your_flame/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.defend_your_flame
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
