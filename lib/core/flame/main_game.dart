cb8826e6d04154baf94252dd388ca799d015772d
Andrew L <anleac@github.com>
2024-03-03 17:46:07 +0100

    Inject in apps strings

--- lib/core/flame/main_game.dart
+++ lib/core/flame/main_game.dart
@@ -1,10 +1,13 @@
 import 'package:defend_your_flame/constants/constants.dart';
 import 'package:defend_your_flame/constants/debug_constants.dart';
+import 'package:defend_your_flame/constants/translations/app_strings.dart';
 import 'package:defend_your_flame/core/flame/managers/sprite_manager.dart';
+import 'package:defend_your_flame/core/storage/game_data.dart';
 import 'package:defend_your_flame/helpers/timestep/debug/timestep_faker.dart';
 import 'package:flame/components.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
 
 class MainGame extends FlameGame {
   // Perhaps these won't ever be needed and can be deleted
@@ -13,6 +16,14 @@ class MainGame extends FlameGame {
 
   // Given this game is within a fixed aspect ratio, this is a simple way to calculate the scaling factor of the games window.
   double get windowScale => windowWidth / Constants.desiredWidth;
+
+  // These are set in `setExternalDependencies`.
+  // Is there a better way to do this?
+  late AppStrings _appStrings;
+  late GameData _gameData;
+
+  AppStrings get appStrings => _appStrings;
+  GameData get gameData => _gameData;
 
   MainGame({required World world, required CameraComponent camera}) : super(world: world, camera: camera);
 
@@ -34,5 +45,13 @@ class MainGame extends FlameGame {
     processLifecycleEvents();
     Flame.images.clearCache();
     Flame.assets.clearCache();
+  }
+
+  void setExternalDependencies(BuildContext context) {
+    // This is where we would set up any external dependencies that the game needs to know about.
+    // Needed for translations and saving data.
+    // This _must_ be called prior to the game being started.
+    _appStrings = AppStrings.of(context);
+    _gameData = GameData.of(context);
   }
 }
