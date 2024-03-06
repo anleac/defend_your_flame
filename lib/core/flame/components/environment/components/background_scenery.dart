dd5b62197d0ce98ceac979c8be23f88a26aab1de
Andrew L <anleac@github.com>
2024-03-06 20:25:24 +0100

    Reshuffle assets

--- lib/core/flame/components/environment/components/background_scenery.dart
+++ lib/core/flame/components/environment/components/background_scenery.dart
@@ -3,7 +3,7 @@ import 'package:defend_your_flame/core/flame/managers/sprite_manager.dart';
 import 'package:flame/components.dart';
 
 class BackgroundScenery extends SpriteComponent {
-  BackgroundScenery() : super(size: Vector2(1440, 746), sprite: SpriteManager.getSprite('background')) {
+  BackgroundScenery() : super(size: Vector2(1440, 746), sprite: SpriteManager.getSprite('environment/background')) {
     scale = Vector2.all(Constants.desiredWidth / 1440);
   }
 }
