fc1efaf641866b3b8f87840809912f41e898ebca
Andrew L <anleac@github.com>
2024-03-19 12:38:22 -0500

    Added a basic button implementation

--- lib/constants/versioning_constants.dart
+++ lib/constants/versioning_constants.dart
@@ -8,8 +8,8 @@ class VersioningConstants {
   static final String version = _getVersion();
 
   static const _majorVersion = 0;
-  static const _minorVersion = 3;
-  static const _patchVersion = 2;
+  static const _minorVersion = 4;
+  static const _patchVersion = 0;
 
   static const _releaseVersion = ReleaseVersion.alpha;
 
--- lib/core/flame/components/hud/components/start_round_button.dart
+++ lib/core/flame/components/hud/components/start_round_button.dart
@@ -1,17 +1,14 @@
+import 'package:defend_your_flame/core/flame/components/hud/abstract_components/text_button.dart';
 import 'package:defend_your_flame/core/flame/components/hud/level_hud.dart';
 import 'package:defend_your_flame/core/flame/main_game.dart';
 import 'package:defend_your_flame/core/flame/managers/text_manager.dart';
 import 'package:flame/components.dart';
-import 'package:flame/events.dart';
-import 'package:flutter/material.dart';
 
-class StartRoundButton extends TextBoxComponent
-    with ParentIsA<LevelHud>, HasGameReference<MainGame>, TapCallbacks, HasVisibility {
+class StartRoundButton extends TextButton with ParentIsA<LevelHud>, HasGameReference<MainGame> {
   StartRoundButton()
       : super(
-          text: '',
-          anchor: Anchor.center,
-          textRenderer: TextManager.smallHeaderRenderer,
+          defaultTextRenderer: TextManager.smallHeaderRenderer,
+          hoveredTextRenderer: TextManager.smallHeaderHoveredRenderer,
         );
 
   @override
@@ -27,29 +24,7 @@ class StartRoundButton extends TextBoxComponent
   }
 
   @override
-  void render(Canvas canvas) {
-    super.render(canvas);
-
-    final paint = Paint()
-      ..color = Colors.white
-      ..strokeWidth = 2
-      ..style = PaintingStyle.stroke;
-
-    final lineStart = Offset(-8, scaledSize.y / 2 + 15);
-    final lineEnd = Offset(scaledSize.x - 20, scaledSize.y / 2 + 15);
-    canvas.drawLine(lineStart, lineEnd, paint);
-  }
-
-  @override
-  bool containsLocalPoint(Vector2 point) {
-    return isVisible && super.containsLocalPoint(point);
-  }
-
-  @override
-  void onTapUp(TapUpEvent event) {
-    super.onTapUp(event);
-
-    // Only on the first round do we want the start game text.
+  void onPressed() {
     text = game.appStrings.startRound;
     parent.startRound();
   }
