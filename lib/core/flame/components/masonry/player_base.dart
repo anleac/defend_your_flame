f836f5d6d0f8ecfa89aefa19977f5ca4aef27190
Andrew L <anleac@github.com>
2024-05-11 19:44:35 +0100

    Add compounding costs for the attack totems

--- lib/core/flame/components/masonry/player_base.dart
+++ lib/core/flame/components/masonry/player_base.dart
@@ -25,7 +25,7 @@ class PlayerBase extends PositionComponent with HasWorldReference<MainWorld>, Ha
   late final FirePit _firePit = FirePit()
     ..position = Vector2(Wall.wallAreaWidth + (baseWidthWithoutWall / 2) - 10, baseHeight / 2 - 10);
 
-  int _gold = DebugConstants.testShopLogic ? 5000 : 10000;
+  int _gold = DebugConstants.testShopLogic ? 5000 : 0;
 
   int get totalGold => _gold;
   bool get destroyed => _wall.health <= 0;
