06b5fb3cc97c4263226bf9d249895b6621fe718a
Andrew L <anleac@github.com>
2024-05-13 12:44:38 +0100

    Tiny update

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:defend_your_flame/constants/constants.dart';
 import 'package:defend_your_flame/core/flame/game_provider.dart';
 import 'package:defend_your_flame/helpers/platform_helper.dart';
 import 'package:defend_your_flame/core/storage/game_data.dart';
@@ -6,6 +7,8 @@ import 'package:flame/flame.dart';
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:html' as html;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,6 +19,8 @@ void main() async {
   if (PlatformHelper.isMobile) {
     await Flame.device.setLandscape();
     await Flame.device.fullScreen();
+  } else if (PlatformHelper.isWeb) {
+    html.document.title = Constants.gameTitle;
   }
 
   runApp(ScopedModel<GameData>(
